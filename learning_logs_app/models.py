ff170dbf18eb97fd1d0621abc711147eb3673877
Mokki <a.mokrzcki@gmail.com>
2020-08-05 22:19:38 +0200

    project base

---
+++ learning_logs_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+# my first model from book Python - instr for programer
+
+class Topic (models.Model):
+    '''Temat poznawany przez uzytkownika'''
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        '''Zwraca reprezentacje modelu w postaci ciagu tekstowego.'''
+        return self.text
