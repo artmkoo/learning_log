9f0ed8a9b415497373d813d4b750e735dab7464e
artmkoo <a.mokrzycki@gmail.com>
2020-09-11 18:05:50 +0200

    PC start 20200911

--- learning_logs_app/views.py
+++ learning_logs_app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+# import dla topics
+from .models import Topic
 
 # Create your views here.
 
 def index (request):
     """Strona główna aplikacji learning log."""
     return render(request, 'learning_logs_app/index.html')
+
+def topics (request):
+    """Wyświetlanie wszystkich tematów"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
